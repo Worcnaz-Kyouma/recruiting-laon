78717c0ea8d9f86223552ee9c651cce05ba7c3f2
Worcnaz Prado <nicolasaprado028@gmail.com>
2025-10-11 20:54:27 -0300

    feat: starintg implementation of generic communication with API

--- recruiting-laon-backend/app/Services/TMDBApiService.php
+++ recruiting-laon-backend/app/Services/TMDBApiService.php
@@ -2,10 +2,21 @@
 
 namespace App\Services;
 
+use Http;
+use Illuminate\Http\Client\Response;
+
+/**
+ * @template T of Media
+ */
 class TMDBApiService {
     private string $baseUrl;
     private int $apiVersion;
     private string $apiKey;
+
+    protected static array $localEntitiesToAPIEntitiesMap = [
+        Movie::class => 'movie',
+        TVShow::class => 'tvshow'
+    ];
     
     public function __construct(
         string $baseUrl,
@@ -15,5 +26,52 @@ class TMDBApiService {
         $this->baseUrl = $baseUrl;
         $this->apiVersion = $apiVersion;
         $this->apiKey = $apiKey;
+    }
+
+    /**
+     * @return array<T>
+     */
+    public function getPopularMedia(): array {
+        $movies = $this->getPopularMedia(Movie::class);
+        $tvShows = $this->getPopularMedia(TVShow::class);
+
+        $medias = array_merge($movies, $tvShows);
+
+        return $medias;
+    }
+
+    /**
+     * @param class-string<T> $mediaType
+     * @return array<T>
+     */
+    private function getPopularMediaGeneric(string $mediaType): array {
+        $apiEntity = self::$localEntitiesToAPIEntitiesMap[$mediaType];
+        
+        $response = $this->fetchAPIData($apiEntity, "popular", []);
+        
+        $data = $response->json();
+        return $data;
+    }
+
+    /**
+     * @param string[] $query
+     */
+    private function fetchAPIData(string $apiEntity, string $endPoint, array $query): Response {
+        $defaultHeaders = [
+            'Authorization' => $this->apiKey,
+            'Accept'        => 'application/json',
+        ];
+        $defaultQuery = [
+            'language' => 'en-US'
+        ];
+
+        $response = Http::withHeaders($defaultHeaders)->get(
+            "$this->baseUrl/$this->apiVersion/$apiEntity/$endPoint", 
+            array_merge($defaultQuery, $query)
+        );
+        if (!$response->successful()) 
+            throw new \Exception('Something went wrong!');
+
+        return $response;
     }
 }
