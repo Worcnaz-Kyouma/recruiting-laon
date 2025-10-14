2e06e941b8f823278f6f38cfac5cb7a4cfcbf0f3
Worcnaz-Kyouma <nicolasaprado028@gmail.com>
2025-10-14 03:00:00 -0300

    refactor: fixing tab sizes

--- recruiting-laon-frontend/app/layout.tsx
+++ recruiting-laon-frontend/app/layout.tsx
@@ -3,32 +3,32 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Laon Catalog",
-  description: "Movies and TV series catalog app",
+    title: "Laon Catalog",
+    description: "Movies and TV series catalog app",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
+return (
     <html lang="en">
-      <body
+    <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    >
         {children}
-      </body>
+    </body>
     </html>
-  );
+);
 }
--- recruiting-laon-frontend/app/movies/page.tsx
+++ recruiting-laon-frontend/app/movies/page.tsx
@@ -1,3 +1,3 @@
 export default function MoviesPage() {
-  return (<></>);
+    return (<></>);
 }
