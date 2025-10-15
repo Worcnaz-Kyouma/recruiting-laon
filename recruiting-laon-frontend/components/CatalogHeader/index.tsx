b7cd074daf36b5c21b8ae9d80b53cc19fe822532
Worcnaz-Kyouma <nicolasaprado028@gmail.com>
2025-10-14 23:59:05 -0300

    feat: more improves in top popular

--- recruiting-laon-frontend/components/CatalogHeader/index.tsx
+++ recruiting-laon-frontend/components/CatalogHeader/index.tsx
@@ -6,13 +6,11 @@ import LoggedUserDropdown from "../LoggedUserDropdown";
 export default function CatalogHeader() {
     const { user } = useUserStore(state => state);
 
-    return <header className="flex items-center justify-between p-4 px-[90px] bg-gray-200 border-b border-gray-300">
+    return <header className="flex items-center justify-between p-6 px-[90px] bg-gray-200 border-b border-gray-300">
         <span>Catalog</span> {/* Change to logo */}
         {user
             ? <LoggedUserDropdown user={user} />
-            : <div className="flex space-x-2 items-center cursor-pointer p-2 px-4 rounded-lg border border-gray-300 text-sm text-nowrap hover:bg-gray-300 transition">
-                <span>Entrar</span>
-            </div>
+            : <button className="cursor-pointer text-base font-medium tracking-widest">ENTRAR</button>
         }
     </header>
 }
